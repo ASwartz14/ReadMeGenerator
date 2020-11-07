461e1ae0ea6915bc0cfd048385bfceaea10814fd
ASwartz14 <asriley144@gmail.com>
2020-11-07 15:27:04 -0800

    HOPEFULLY adds badges....experimental

--- index.js
+++ index.js
@@ -20,16 +20,30 @@ const questions = [{
         name: "install"
     },
     {
-        type: "checkbox",
+        type: "list",
         message: "Please select the licence you wish to use for this project.",
         name: "license",
-        choices: [
-            "MIT",
-            "GNU GPLv3",
-            "Mozilla PL 2.0",
-            "Apache 2.0",
-            "Boost Software 1.0",
-            "Unlicense/Other"
+        choices: [{
+                name: "MIT",
+                value: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+            },
+            {
+                name: "GNU GPLv3",
+                value: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+            },
+            {
+                name: "Mozilla PL 2.0",
+                value: "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)"
+            },
+            {
+                name: "Apache 2.0",
+                value: "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+            },
+            {
+                name: "Boost Software 1.0",
+                value: "[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+            },
+            "Unlicense/Other",
         ]
     },
     {
